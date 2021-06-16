ac7899aab3206302eca4f944855e1842910de045
asandyy <sandyli110@gmail.com>
2021-06-16 09:39:15 -0700

    updating file

--- Documents/Program-Assignments/fundamentals.py
+++ Documents/Program-Assignments/fundamentals.py
@@ -1,1 +1,20 @@
-print("Hello World!");
+# prints Hello World!
+print("Hello World!")
+
+# 1: Practicing Var & Data types
+name = "LeBron" # assigns LeBron to name
+age = 35    # age variable
+salary = 37.44  # assigns salary
+plays_basketball = True # assigns t/f variable
+jersey_number = "23" # assigns jersey number
+
+# 2
+car_name = "Toyota"
+x = 50
+first_name = "Sandy"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
